54f12ee43d8d3258ee46d7d81291102849664c64
ademola <fioyedemola1@gmil.com>
2021-04-25 13:45:19 +0100

    fixed test

--- Python_data/day1/Checks/checker.py
+++ Python_data/day1/Checks/checker.py
@@ -1,13 +1,17 @@
 from functools import wraps
 
 
-def checks(item=None):
+def checks(item):
     def inner(func):
         @wraps(func)
-        def wrapper(value,*args, **kwargs):
+        def wrapper(value=None,*args):
+            if value == None:
+                print(func.__name__, 'has no value')
+                return 
             print('*' * 10)
-            ans = func(value, *args, **kwargs)
+            ans = func(value)
             if item != type(ans):
+                print(ans, type(ans))
                 print('Your answer is  incorrect')
             else:
                 print('Test passed')
--- Python_data/day1/excercise.py
+++ Python_data/day1/excercise.py
@@ -1,49 +1,57 @@
 from Checks.checker import checks
 
-# insert a string
-
-
-
 @checks(str)
 def string_checker(value,*args):
     ans = value
     return ans
 
 @checks(int)
-def integer_checker(value,*args):
+def integer_checker(value ,*args):
     ans = value
     return ans
 
 
 @checks(bool)
-def boolean_checker(value,*args):
+def boolean_checker(value ,*args):
     ans = value
     return ans
 
 
 @checks(list)
-def List_checker(value,*args):
+def List_checker(value ,*args):
     ans = value
     return ans
 
 
 @checks(dict)
-def dictionary_checker(value,*args):
+def dictionary_checker(value = None,*args):
     ans = value
     return ans
 
 
 @checks(tuple)
-def tuple_checker(value,*args):
+def tuple_checker(value = None,*args):
     ans = value
     return ans
 
 
 @checks(set)
-def set_checker(value,*args):
+def set_checker(value = None,*args):
     ans = value
     return ans
 
+# -----------------------------------------------xxxxx Your answers xxxxx---------------->
 
 
-set_checker(12,3,4)
+
+
+
+
+if __name__ == '__main__':
+    string_checker()             #`string`
+    boolean_checker()             #`boolean
+    integer_checker()           #`integer
+    List_checker()               #list
+    dictionary_checker()          # Dictionary
+    tuple_checker()               #tuple
+    set_checker()                 #set
