be67244ca3b1c85bd392c0f8e537314f481302c1
ademola <fioyedemola1@gmil.com>
2021-05-14 01:23:19 +0100

    day9

---
+++ module/functions.py
@@ -0,0 +1,15 @@
+# pure
+
+
+def example():
+    print('example')
+
+
+class Animal:  # class
+    leg = 2   
+
+    def printer(self):  # method
+        return 'Animal'
+
+
+
---
+++ module/main.py
@@ -0,0 +1,7 @@
+from module import functions as fn
+
+
+
+fn.example()
+a =fn.Animal().printer()
+print(a)
